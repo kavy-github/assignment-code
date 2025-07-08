c74c5c6bdc6002a98ccfec594efb1081359b0af1
kavy-github <143808999+kavy-github@users.noreply.github.com>
2025-07-08 15:51:17 +0530

    Add files via upload

---
+++ Q4.py
@@ -0,0 +1,8 @@
+length = int(input("Enter the length: "))
+breadth = int(input("Enter the breadth: "))
+
+area = length * breadth
+perimeter = 2 * (length + breadth)
+
+print(f"Area: {area}")
+print(f"Perimeter: {perimeter}")
